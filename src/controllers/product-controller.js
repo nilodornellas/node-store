7372db53e1f96414646b17e92ce56032d7d8bc4a
Ian Nilo Dornellas <iannilo01@gmail.com>
2023-09-13 19:29:47 -0300

    implement: update and delete products on database

--- src/controllers/product-controller.js
+++ src/controllers/product-controller.js
@@ -61,13 +61,35 @@ exports.post = (req, res, next)=> {
 }
 
 exports.put = (req, res, next)=> {
-    const id = req.params.id;
-    res.status(200).send({
-        id: id, 
-        item: req.body
+    Product.findByIdAndUpdate(req.params.id, {
+        $set: {
+            title: req.body.title,
+            description: req.body.description,
+            price: req.body.price,
+            slug: req.body.slug
+        }
+    }).then(x => {
+        res.status(201).send({
+            message: 'Produto atualizado com sucesso!'
+        });
+    }).catch(e => {
+        res.status(400).send({
+            message: 'Falha ao atualizar o produto',
+            data: e
+        });
     });
 }
 
 exports.delete = (req, res, next)=> {
-    res.status(200).send(req.body);
+    Product.findOneAndRemove(req.body.id)
+    .then(x => {
+        res.status(200).send({
+            message: 'Produto removido com sucesso!'
+        });
+    }).catch(e => {
+        res.status(400).send({
+            message: 'Falha ao remover o produto',
+            data: e
+        });
+    });
 }
